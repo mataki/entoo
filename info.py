133a68af81a00da70e4ef3504dbbad6bdc75bf51
mat_aki <matsumura.aki@gmail.com>
2009-06-01 03:09:09 +0900

    init

---
+++ info.py
@@ -0,0 +1,16 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class InfoHandler(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write(template.render('info.html', {}))
+
+application = webapp.WSGIApplication([('/', InfoHandler)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
