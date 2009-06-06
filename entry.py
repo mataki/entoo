bbd81ec6ac930f3e4d9e0246fe154becd2623b1f
mat_aki <matsumura.aki@gmail.com>
2009-06-06 11:14:41 +0900

    enable update data

--- entry.py
+++ entry.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -38,20 +23,22 @@ class EntriesHandler(webapp.RequestHandler):
       self.response.out.write(template.render('index.html',{'entries':entries}))
 
   def post(self):
-    entry = Entry()
-    entry.user =  users.get_current_user()
-    entry.target = self.request.get('target')
-    entry.email = self.request.get('email')
-    entry.put()
-
-    self.redirect('/entries')
-
-  def put(self):
     key = self.request.get('key')
     if key:
+      entry = Entry.get(key)
+      entry.target = self.request.get('target')
+      entry.email = self.request.get('email')
+      entry.put()
+
       self.redirect("/entries?key=%s" % key)
     else:
-      self.response.out.write("BUT REQUEST")
+      entry = Entry()
+      entry.user =  users.get_current_user()
+      entry.target = self.request.get('target')
+      entry.email = self.request.get('email')
+      entry.put()
+
+      self.redirect('/entries')
 
 application = webapp.WSGIApplication([('/entries', EntriesHandler)],
                                      debug=True)
