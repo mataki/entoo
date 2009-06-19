252debfaa1907e6a06556c6b4c0075fcd76fd5ab
mat_aki <matsumura.aki@gmail.com>
2009-06-19 21:15:32 +0900

    send email with multi twit

--- check.py
+++ check.py
@@ -20,23 +20,25 @@ class CheckHandler(webapp.RequestHandler):
     url = "http://twitter.com/statuses/user_timeline/train_kanto.xml"
     result = urlfetch.fetch(url, '', urlfetch.GET)
 
-    send_obj = []
     elem = fromstring(result.content)
-    entries = Entry.all()
+
+    twits = []
     for s in elem.findall('.//status'):
       text = s.findtext('./text').encode('utf-8')
-      for entry in entries:
-        if text.find(entry.target.encode('utf-8')) > -1:
-          send_obj.append({'entry':entry, 'tweet':text})
+      twits.append(text)
 
-    for s in send_obj:
-      mail.send_mail(sender = 'matsumura.aki+entoo@gmail.com',
-                     to = s['entry'].email,
-                     subject = 'Entoo %s' % s['entry'].target,
-                     body = s['tweet'],
-                     )
+    entries = Entry.all()
+    bodys = []
+    for entry in entries:
+      body = ''
+      for t in twits:
+        if t.find(entry.target.encode('utf-8')) > -1:
+          body = body + t + '\n\n'
+      if body and 0 < len(body.strip()):
+        bodys.append(body)
+        mail.send_mail('entoo@mat-aki.net', entry.email, 'Entoo %s' % entry.target, body)
 
-    self.response.out.write(template.render('check.html',{'send_obj':send_obj}))
+    self.response.out.write(template.render('check.html',{'send_obj':bodys}))
 
 application = webapp.WSGIApplication([('/check', CheckHandler)],
                                      debug=True)
